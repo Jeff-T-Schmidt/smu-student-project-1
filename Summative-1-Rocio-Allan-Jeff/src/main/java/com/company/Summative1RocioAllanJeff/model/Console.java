ffe07038faaee2ec4144c3e529587c34306debc8
ro_g <97912154+Ro-Galvan@users.noreply.github.com>
2022-10-18 11:34:40 -0400

    started on console class

--- Summative-1-Rocio-Allan-Jeff/src/main/java/com/company/Summative1RocioAllanJeff/model/Console.java
+++ Summative-1-Rocio-Allan-Jeff/src/main/java/com/company/Summative1RocioAllanJeff/model/Console.java
@@ -1,4 +1,13 @@
 package com.company.Summative1RocioAllanJeff.model;
 
+import javax.persistence.Column;
+
 public class Console {
+
+    @Column(name="first_name")
+    private String firstName;
+    @Column(name="last_name")
+    private String lastName;
+    private String company;
+    private String phone;
 }
