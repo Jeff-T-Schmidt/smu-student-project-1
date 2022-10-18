d3caf269fea0a7a91fd52f9fa3138ca79fc744e3
Jeff Schmidt <jeff.t.schmidt@gmail.com>
2022-10-18 07:42:40 -0700

    Initial setup

---
+++ Summative-1-Rocio-Allan-Jeff/src/main/java/com/company/Summative1RocioAllanJeff/Summative1RocioAllanJeffApplication.java
@@ -0,0 +1,13 @@
+package com.company.Summative1RocioAllanJeff;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Summative1RocioAllanJeffApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Summative1RocioAllanJeffApplication.class, args);
+	}
+
+}
---
+++ Summative-1-Rocio-Allan-Jeff/src/test/java/com/company/Summative1RocioAllanJeff/Summative1RocioAllanJeffApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.Summative1RocioAllanJeff;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Summative1RocioAllanJeffApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
