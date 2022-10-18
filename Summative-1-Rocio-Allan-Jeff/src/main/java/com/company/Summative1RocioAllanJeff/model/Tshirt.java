73d396e7efcf2c145948cef63e360e16a4d240ac
Jeff Schmidt <jeff.t.schmidt@gmail.com>
2022-10-18 08:21:37 -0700

    Big push

--- Summative-1-Rocio-Allan-Jeff/src/main/java/com/company/Summative1RocioAllanJeff/model/Tshirt.java
+++ Summative-1-Rocio-Allan-Jeff/src/main/java/com/company/Summative1RocioAllanJeff/model/Tshirt.java
@@ -1,4 +1,15 @@
 package com.company.Summative1RocioAllanJeff.model;
 
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+
 public class Tshirt {
+    @NotEmpty(message = "You must supply a value for artist")
+    private String artist;
+    @NotEmpty(message = "You must supply a value for album")
+    private String album;
+    @NotEmpty(message = "You must supply a value for year")
+    @Size(min = 4, max = 4, message = "Year must be exactly 4 digits")
+    private String year;
+    private int id;
 }
