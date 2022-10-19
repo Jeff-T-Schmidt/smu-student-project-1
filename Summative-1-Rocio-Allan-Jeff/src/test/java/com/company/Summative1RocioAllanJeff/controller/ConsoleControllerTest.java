40beec6d3407ee58619bf740488857add29c8b24
ro_g <97912154+Ro-Galvan@users.noreply.github.com>
2022-10-19 16:42:17 -0400

    mockmvc tests in progress

--- Summative-1-Rocio-Allan-Jeff/src/test/java/com/company/Summative1RocioAllanJeff/controller/ConsoleControllerTest.java
+++ Summative-1-Rocio-Allan-Jeff/src/test/java/com/company/Summative1RocioAllanJeff/controller/ConsoleControllerTest.java
@@ -15,7 +15,9 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 import static org.junit.Assert.*;
 import static org.mockito.Mockito.doReturn;
@@ -30,25 +32,36 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 public class ConsoleControllerTest {
 
     @Autowired
-    private MockMvc mockMvc;
+    MockMvc mockMvc;
 
     @MockBean
-    private ConsoleRepository repository;
+    ConsoleRepository repository;
     ObjectMapper mapper = new ObjectMapper();
     Console inputConsole;
     Console outputConsole;
+    Console outputConsole2;
+    List<Console> allConsoles = new ArrayList<>();
 
-    List<Console> consoleList;
+    String allConsolesJson;
 
     @Before
      public void setUp() throws Exception {
         //        input - setting arguments
          inputConsole = new Console("xbox","Sony","64gb","idk",199.99f,1);
-
-        //        output + id to mimic postman
+        //        outputs + id to mimic postman
         outputConsole = new Console("xbox","Sony","64gb","idk",199.99f,1);
         outputConsole.setConsoleId(1);
-        doReturn(outputConsole).when(repository).save(inputConsole);
+
+
+        outputConsole2 = new Console("playstation","Sony","84gb","Jaguar",249.99f,1);
+        outputConsole2.setConsoleId(2);
+        allConsoles.add(inputConsole);
+        allConsolesJson = mapper.writeValueAsString(allConsoles);
+
+        allConsoles = new ArrayList<>(Arrays.asList(
+           outputConsole,
+           outputConsole2     
+        ));
     }
 
     // Create/POST a console
@@ -57,6 +70,8 @@ public class ConsoleControllerTest {
 
         String inputConsoleJson = mapper.writeValueAsString(inputConsole);
         String outputConsoleJson = mapper.writeValueAsString(outputConsole);
+
+        doReturn(outputConsole).when(repository).save(inputConsole);
 
         // Act
         mockMvc.perform(post("/consoles")
@@ -69,27 +84,36 @@ public class ConsoleControllerTest {
     }
 
     // Read/GET ALL consoles
-
-//    TODO: not sure if it is 100% correct
     @Test
-    public void shouldGetAllConsolesOnGetRequest() throws Exception {
-        List<Console> outputConsole = new ArrayList<>();
-        String outputConsoleJson = mapper.writeValueAsString(outputConsole);
+    public void shouldReturnAllConsolesOnGetRequest() throws Exception {
+        String outputJson = mapper.writeValueAsString(allConsoles);
 
-        doReturn(outputConsole).
+//        doReturn(allConsoles).when(repository).findAll();
+        doReturn(allConsoles).
                 when // conditional
                 (repository).findAll(); //method we want to test
 
-        ResultActions result = mockMvc.perform(
+//        ResultActions result = mockMvc.perform(
+            mockMvc.perform(
                         get("/consoles"))
                 .andDo(print())
                 .andExpect(status().isOk())
-                .andExpect((content().json(outputConsoleJson)));
-
+                .andExpect((content().json(outputJson)));
     }
 
+//    TODO: Read/GET 1 console--not working need to fix
+    public void shouldReturnConsoleById() throws Exception {
 
-//    TODO: Read/GET 1 console
+        String outputJson = mapper.writeValueAsString(outputConsole2);
+
+        doReturn(Optional.of(outputConsole2)).when(repository).findById(2);
+
+        mockMvc.perform(
+                get("/consoles/2"))
+                .andDo(print())
+            .andExpect(status().isOk())
+            .andExpect((content().json(outputJson)));
+}
 
 //    TODO: Update/PUT a console
 //    TODO: Delete a console
