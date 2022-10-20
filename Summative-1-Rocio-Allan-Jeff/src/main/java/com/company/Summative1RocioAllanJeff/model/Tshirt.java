4c06ddafbd0f890c290e5a037eb012d34dec4849
Jeff Schmidt <jeff.t.schmidt@gmail.com>
2022-10-20 15:33:40 -0700

    Dan the Man - helping us with our Service Layer

--- Summative-1-Rocio-Allan-Jeff/src/main/java/com/company/Summative1RocioAllanJeff/model/Tshirt.java
+++ Summative-1-Rocio-Allan-Jeff/src/main/java/com/company/Summative1RocioAllanJeff/model/Tshirt.java
@@ -8,7 +8,7 @@ import javax.validation.constraints.Size;
 import java.util.Objects;
 @Entity
 @JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
-@Table(name = "tshirt")
+@Table(name = "t_shirt")
 public class Tshirt {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
